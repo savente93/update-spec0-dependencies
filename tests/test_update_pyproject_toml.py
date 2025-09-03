7602cf7a89ba13082c93e9adf58c698de34cf8b0
Sam Vente <savente93@proton.me>
2025-09-03 15:38:23 +0200

    feat: implement updating actual project dependencies

---
+++ tests/test_update_pyproject_toml.py
@@ -0,0 +1,11 @@
+from update_spec0_dependencies.parsing import read_schedule, read_toml
+from update_spec0_dependencies import update_pyproject_toml
+
+
+def test_update_pyproject_toml():
+    expected = read_toml("tests/test_data/pyproject_updated.toml")
+    pyproject_data = read_toml("tests/test_data/pyproject.toml")
+    test_schedule = read_schedule("tests/test_data/test_schedule.json")
+    update_pyproject_toml(pyproject_data, test_schedule)
+
+    assert pyproject_data == expected
